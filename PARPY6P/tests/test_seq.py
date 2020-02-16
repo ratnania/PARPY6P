442472dd7a76c9869f8cbc7c927c2f10e0baecc6
ratnania <ratnaniahmed@gmail.com>
2020-02-16 23:07:08 +0100

    update tests

--- PARPY6P/tests/test_seq.py
+++ PARPY6P/tests/test_seq.py
@@ -1,5 +1,15 @@
 from PARPY6P import meshpart
 
+# ... get the mesh directory
+try:
+    mesh_dir = os.environ['MESH_DIR']
+
+except:
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    base_dir = os.path.join(base_dir, '..', '..')
+    mesh_dir = os.path.join(base_dir, 'mesh')
+# ...
 
 def test_mesh_1():
-    meshpart.MeshPartitioning.MeshPart(2, "mesh.msh")
+    filename = os.path.join(mesh_dir, "mesh.msh")
+    meshpart.MeshPartitioning.MeshPart(2, filename)
